794e3500733bfd9032fc39b2049968591559a5a1
Robert Zaremba <robert@zaremba.ch>
2025-06-24 02:28:03 +0200

    link D1 DB and use gen worker Env type
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- packages/btcindexer/src/routertype.ts
+++ packages/btcindexer/src/routertype.ts
@@ -1,12 +1,7 @@
 import type { IRequest, RouterType } from 'itty-router';
 
-// Cloudflare env object
-type Environment = {
-	KV: KVNamespace;
-};
-
 // list of argument types provided to the router handlers
-export type CFArgs = [Environment, ExecutionContext];
+export type CFArgs = [Env, ExecutionContext];
 
 // itty router type for this worker
 export type AppRouter = RouterType<IRequest, CFArgs>;

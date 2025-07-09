cd2b8a8778765ec4f5186121b318b5d432b29bfd
sczembor <stanislaw.czembor@gmail.com>
2025-07-09 13:11:58 +0200

    disable lint rule
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,4 +11,10 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  {
+    // disable specific rules
+    rules: {
+      "@typescript-eslint/no-extraneous-class": "off",
+    },
+  }
 );

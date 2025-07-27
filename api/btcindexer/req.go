9d9125214c9dc24a9ea2ca93e971eb41de0c94f8
Robert Zaremba <robert@zaremba.ch>
2025-07-27 22:06:13 +0300

    review
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- api/btcindexer/req.go
+++ api/btcindexer/req.go
@@ -2,10 +2,12 @@
 
 package btcindexer
 
+// PutBlock wraps serialized Bitcoin block (using Bitcoin core encoding) and the block height.
 type PutBlock struct {
 	Height int64 `msg:"height"`
 	// Block is bitcoin core encoding of a Bitcoin block
 	Block []byte `msg:"block"`
 }
 
+// PutBlocksReq is the type expected by the PUT bitcoin/blocks endpoint
 type PutBlocksReq []PutBlock

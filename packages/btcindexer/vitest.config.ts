d484d7814146789e4dd4914ec6dfb45cdf2aa1d6
Rayane Charif <rayane.charif@gonative.cc>
2025-09-17 09:57:20 +0200

    Corrected test errors
    Signed-off-by: Rayane Charif <rayane.charif@gonative.cc>

--- packages/btcindexer/vitest.config.ts
+++ packages/btcindexer/vitest.config.ts
@@ -1,27 +1,7 @@
 import { defineConfig } from "vitest/config";
-import { defineWorkersProject } from "@cloudflare/vitest-pool-workers/config";
 
-export default defineConfig(
-	defineWorkersProject({
-		test: {
-			globals: true,
-			poolOptions: {
-				workers: {
-					wrangler: { configPath: "./wrangler.jsonc" },
-					miniflare: {
-						compatibilityDate: "2025-06-20",
-						compatibilityFlags: ["nodejs_compat"],
-					},
-				},
-			},
-		},
-		define: {
-			global: "globalThis",
-		},
-		resolve: {
-			alias: {
-				"node:crypto": "crypto",
-			},
-		},
-	}),
-);
+export default defineConfig({
+	test: {
+		globals: true,
+	},
+});

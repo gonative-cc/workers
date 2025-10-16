6d467becba402399eae955795cd6c77c4af17f2a
sczembor <stanislaw.czembor@gmail.com>
2025-10-16 11:34:49 +0200

    feat: conditional logging in tests
    - Add conditional logging to tests based on ENABLE_LOGS env var
    - Update README with instructions on how to enable logs
    - Move bunfig.toml to btcindexer package

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,11 +3,13 @@ import { beforeAll, afterAll, vi } from "bun:test";
 const originalConsole = { ...console };
 
 beforeAll(() => {
-	for (const key of Object.keys(originalConsole)) {
-		const m = key as keyof Console;
-		if (typeof originalConsole[m] === "function") {
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			(console[m] as any) = vi.fn();
+	if (process.env.ENABLE_LOGS !== "1") {
+		for (const key of Object.keys(originalConsole)) {
+			const m = key as keyof Console;
+			if (typeof originalConsole[m] === "function") {
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				(console[m] as any) = vi.fn();
+			}
 		}
 	}
 });

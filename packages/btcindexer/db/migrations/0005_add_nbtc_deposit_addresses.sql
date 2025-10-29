1b5a83aa54d79e770c101ad236830619a274aeeb
sczembor <43810037+sczembor@users.noreply.github.com>
2025-10-29 14:38:31 +0100

    Apply suggestions from code review
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>
    Signed-off-by: sczembor <43810037+sczembor@users.noreply.github.com>

--- packages/btcindexer/db/migrations/0005_add_nbtc_deposit_addresses.sql
+++ packages/btcindexer/db/migrations/0005_add_nbtc_deposit_addresses.sql
@@ -1,8 +1,8 @@
 
-CREATE TABLE IF NOT EXISTS nbtc_deposit_addresses (
+CREATE TABLE IF NOT EXISTS nbtc_addresses (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   btc_network TEXT NOT NULL,
   sui_network TEXT NOT NULL,
   nbtc_pkg TEXT NOT NULL,
-  address TEXT NOT NULL,
+  btc_address TEXT NOT NULL,
 );

e2b63a1609b5f3ca7d324d4218e1547090edc93d
sczembor <stanislaw.czembor@gmail.com>
2025-11-25 14:26:48 +0100

    add itty router
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- packages/block-ingestor/src/index.ts
+++ packages/block-ingestor/src/index.ts
@@ -1,3 +1,4 @@
+import { Router } from "itty-router";
 import { PutBlocksReq } from "./api/put-blocks";
 import { handleIngestBlocks } from "./ingest";
 import type { BtcIndexerRpcI } from "../../btcindexer/src/rpc-interface";
@@ -10,30 +11,35 @@ interface Env {
 	ENVIRONMENT: string;
 }
 
+const router = Router();
+
+router.put("/bitcoin/blocks", async (request, env: Env) => {
+	try {
+		const blocks = PutBlocksReq.decode(await request.arrayBuffer());
+		await handleIngestBlocks(blocks, env.BtcBlocks, env.BlockQueue);
+		return new Response("Blocks ingested successfully", { status: 200 });
+	} catch (e) {
+		console.error("Failed to ingest blocks", e);
+		const message = e instanceof Error ? e.message : "Failed to process request";
+		return new Response(message, { status: 500 });
+	}
+});
+
+router.get("/bitcoin/latest-height", async (_request, env: Env) => {
+	try {
+		const result = await env.BtcIndexer.latestHeight();
+		return Response.json(result);
+	} catch (e) {
+		console.error("Failed to get latest height via RPC", e);
+		return new Response("Internal Error", { status: 500 });
+	}
+});
+
+router.all("*", () => new Response("Not Found", { status: 404 }));
+
 export default {
 	async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
 		// TODO: add authentication method here
-		const url = new URL(request.url);
-		if (request.method === "PUT" && url.pathname === "/bitcoin/blocks") {
-			try {
-				const blocks = PutBlocksReq.decode(await request.arrayBuffer());
-				await handleIngestBlocks(blocks, env.BtcBlocks, env.BlockQueue);
-				return new Response("Blocks ingested successfully", { status: 200 });
-			} catch (e) {
-				console.error("Failed to ingest blocks", e);
-				const message = e instanceof Error ? e.message : "Failed to process request";
-				return new Response(message, { status: 500 });
-			}
-		}
-		if (request.method === "GET" && url.pathname === "/bitcoin/latest-height") {
-			try {
-				const result = await env.BtcIndexer.latestHeight();
-				return Response.json(result);
-			} catch (e) {
-				console.error("Failed to get latest height via RPC", e);
-				return new Response("Internal Error", { status: 500 });
-			}
-		}
-		return new Response("Not Found", { status: 404 });
+		return router.handle(request, env);
 	},
 };

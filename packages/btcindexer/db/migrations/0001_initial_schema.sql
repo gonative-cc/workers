7626c9806d3eea5da7d33b42f8eef1270beadc80
sczembor <stanislaw.czembor@gmail.com>
2025-12-01 18:10:10 +0100

    normalize db schema
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- packages/btcindexer/db/migrations/0001_initial_schema.sql
+++ packages/btcindexer/db/migrations/0001_initial_schema.sql
@@ -15,7 +15,8 @@ CREATE INDEX IF NOT EXISTS btc_blocks_is_scanned_height ON btc_blocks (is_scanne
 
 -- This table tracks the nBTC deposit txs (minting)
 CREATE TABLE IF NOT EXISTS nbtc_minting (
-	tx_id TEXT NOT NULL,
+	tx_id TEXT NOT NULL PRIMARY KEY,
+	config_id INTEGER NOT NULL,
 	vout INTEGER NOT NULL,
 	block_hash TEXT,
 	block_height INTEGER,
@@ -26,14 +27,10 @@ CREATE TABLE IF NOT EXISTS nbtc_minting (
 	updated_at INTEGER NOT NULL, -- timestamp_ms
 	sui_tx_id TEXT,
 	retry_count INTEGER NOT NULL DEFAULT 0,
-	nbtc_pkg TEXT NOT NULL,
-	sui_network TEXT NOT NULL,
-	btc_network TEXT NOT NULL,
-	deposit_address TEXT NOT NULL,
-	PRIMARY KEY (tx_id, vout)
+	FOREIGN KEY (config_id) REFERENCES nbtc_configs(id)
 ) STRICT;
 
-CREATE INDEX IF NOT EXISTS nbtc_minting_status ON nbtc_minting (status);
+CREATE INDEX IF NOT EXISTS nbtc_minting_status ON nbtc_minting (config_id, status);
 CREATE INDEX IF NOT EXISTS nbtc_minting_sui_recipient ON nbtc_minting (sui_recipient, created_at);
 
 -- nbtc_withdrawal table tracks nBTC withdraw transactions from SUI
@@ -59,49 +56,49 @@ CREATE INDEX IF NOT EXISTS nbtc_withdraw_sender ON nbtc_withdrawal (sender, reci
 -- 6 = confirmations (here user technically already has the funds)
 
 -- This table links a Bitcoin transaction ID to its sender addresses.
-CREATE TABLE IF NOT EXISTS nbtc_sender_deposits (
+CREATE TABLE IF NOT EXISTS nbtc_deposit_senders (
 	tx_id TEXT NOT NULL,
 	sender TEXT NOT NULL,
-	PRIMARY KEY (sender, tx_id)
+	PRIMARY KEY (sender, tx_id),
+	FOREIGN KEY (tx_id) REFERENCES nbtc_minting(tx_id) ON DELETE CASCADE
 ) STRICT;
 
--- This table holds the deposit addresses for nBTC.
-CREATE TABLE IF NOT EXISTS nbtc_addresses (
+-- This table holds the config for nBTC.
+CREATE TABLE IF NOT EXISTS nbtc_configs (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	btc_network TEXT NOT NULL,
 	sui_network TEXT NOT NULL,
 	nbtc_pkg TEXT NOT NULL,
-	btc_address TEXT NOT NULL,
+	deposit_address TEXT NOT NULL,
 	is_active INTEGER NOT NULL DEFAULT TRUE,
-	UNIQUE(btc_address, btc_network)
+	UNIQUE(sui_network, btc_network, nbtc_pkg, deposit_address)
 ) STRICT;
 
 CREATE TABLE IF NOT EXISTS nbtc_utxos ( -- TODO: normalise the database (foreign key to the nbtc_addresses table)
-	sui_id TEXT NOT NULL,
+	sui_id TEXT NOT NULL PRIMARY KEY,
+	config_id INTEGER NOT NULL,
 	dwallet_id TEXT NOT NULL,
 	txid TEXT NOT NULL, -- Bitcoin transaction ID
 	vout INTEGER NOT NULL,
 	amount_sats INTEGER NOT NULL,
 	script_pubkey BLOB NOT NULL,
-	nbtc_pkg TEXT NOT NULL,
-	sui_network TEXT NOT NULL,
 	status TEXT NOT NULL DEFAULT 'available', -- 'available', 'locked', 'spent' TODO: lets remove the 'spent' utxos after some time?
 	locked_until INTEGER,
-	PRIMARY KEY (sui_id)
+	FOREIGN KEY (config_id) REFERENCES nbtc_configs(id)
 ) STRICT;
 
-CREATE INDEX IF NOT EXISTS idx_utxos_selection ON nbtc_utxos(nbtc_pkg, sui_network, status, amount_sats);
+CREATE INDEX IF NOT EXISTS idx_utxos_selection ON nbtc_utxos(config_id, status, amount_sats);
 CREATE INDEX IF NOT EXISTS idx_nbtc_utxos_txid_vout ON nbtc_utxos(txid, vout);
 
 CREATE TABLE IF NOT EXISTS nbtc_redeem_requests (
 	redeem_id TEXT NOT NULL PRIMARY KEY,
+	config_id INTEGER NOT NULL,
 	redeemer TEXT NOT NULL,
 	recipient_script BLOB NOT NULL, -- script pubkey
 	amount_sats INTEGER NOT NULL,
 	created_at INTEGER NOT NULL,
 	status TEXT NOT NULL DEFAULT 'pending', -- 'pending', 'proposed', 'signed', 'broadcasted'
-	nbtc_pkg TEXT NOT NULL, -- TODO: nbtc_pkg and sui_network has to be in a separate table
-	sui_network TEXT NOT NULL
+	FOREIGN KEY (config_id) REFERENCES nbtc_configs(id)
 ) STRICT;
 
 CREATE TABLE IF NOT EXISTS indexer_state ( -- TODO: maybe we should just use key-value here?

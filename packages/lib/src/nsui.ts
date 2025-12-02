cb6690e429f798ed387e6974861ca65bf8a8d1c7
Rayane Charif <rayane.charif@gonative.cc>
2025-12-02 12:29:09 +0100

    moved sui config for gql to nsui.ts
    Signed-off-by: Rayane Charif <rayane.charif@gonative.cc>

--- packages/lib/src/nsui.ts
+++ packages/lib/src/nsui.ts
@@ -1,5 +1,12 @@
 export type SuiNet = "testnet" | "mainnet" | "devnet" | "localnet";
 
+export const SUI_GRAPHQL_URLS: Record<SuiNet, string> = {
+	mainnet: "https://sui-mainnet.mystenlabs.com/graphql",
+	testnet: "https://sui-testnet.mystenlabs.com/graphql",
+	devnet: "https://sui-devnet.mystenlabs.com/graphql",
+	localnet: "http://localhost:9125/graphql",
+};
+
 export interface NbtcPkg {
 	nbtcPkg: string;
 	suiNetwork: SuiNet;
--- packages/sui-indexer/src/config.ts
+++ packages/sui-indexer/src/config.ts
@@ -1,8 +1,1 @@
-import type { SuiNet } from "@gonative-cc/lib/nsui";
-
-export const SUI_NETWORK_URLS: Record<SuiNet, string> = {
-	mainnet: "https://sui-mainnet.mystenlabs.com/graphql",
-	testnet: "https://sui-testnet.mystenlabs.com/graphql",
-	devnet: "https://sui-devnet.mystenlabs.com/graphql",
-	localnet: "TODO",
-};
+export { SUI_GRAPHQL_URLS as SUI_NETWORK_URLS } from "@gonative-cc/lib/nsui";

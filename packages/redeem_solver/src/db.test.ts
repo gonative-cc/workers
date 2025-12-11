e259917fc1f93ec6b9b37e4834434a898ed568a4
Rayane Charif <rayane.charif@gonative.cc>
2025-12-11 14:56:52 +0100

    removed comments
    Signed-off-by: Rayane Charif <rayane.charif@gonative.cc>

--- packages/redeem_solver/src/db.test.ts
+++ packages/redeem_solver/src/db.test.ts
@@ -2,7 +2,6 @@ import * as path from "path";
 import { readdir } from "fs/promises";
 import { D1Database } from "@cloudflare/workers-types";
 
-// Point to btcindexer's migrations since redeem_solver shares the same database schema
 const MIGRATIONS_PATH = path.resolve(__dirname, "../../btcindexer/db/migrations");
 
 export async function initDb(db: D1Database) {

8b40ca30e29cef9ef49863f026ac634b6aed9a7f
Rayane Charif <rayane.charif@gonative.cc>
2025-12-13 10:32:26 +0100

    Moved db.test.ts into common package for test setup in lib
    Signed-off-by: Rayane Charif <rayane.charif@gonative.cc>

--- packages/btcindexer/src/db.test.ts
+++ packages/btcindexer/src/db.test.ts
@@ -1,25 +1,9 @@
 import * as path from "path";
-import { readdir } from "fs/promises";
 import { D1Database } from "@cloudflare/workers-types";
+import { initDb as initDbShared } from "@gonative-cc/lib/common-setup/db.test";
 
 const MIGRATIONS_PATH = path.resolve(__dirname, "../db/migrations");
 
 export async function initDb(db: D1Database) {
-	const migrationFiles = await readdir(MIGRATIONS_PATH);
-	migrationFiles.sort();
-
-	for (const filename of migrationFiles) {
-		if (filename.endsWith(".sql")) {
-			const file = Bun.file(path.join(MIGRATIONS_PATH, filename));
-			const migration = await file.text();
-			const cleanedMigration = migration
-				.replace(/--.*/g, "")
-				.replace(/\n/g, " ")
-				.replace(/\s{2,}/g, " ")
-				.trim();
-			if (cleanedMigration.length > 0) {
-				await db.exec(cleanedMigration);
-			}
-		}
-	}
+	await initDbShared(db, MIGRATIONS_PATH);
 }

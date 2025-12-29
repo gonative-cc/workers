caa140e3fb2054d1ca928341c7cdd31d2a9a115a
Vu Vo <vu.voth@gmail.com>
2025-12-29 19:06:16 +0700

    update
    Signed-off-by: Vu Vo <vu.voth@gmail.com>

--- packages/btcindexer/scripts/config.ts
+++ packages/btcindexer/scripts/config.ts
@@ -24,13 +24,13 @@ export const SETUPS: Record<EnvName, Config> = {
 			{
 				btc_network: "regtest",
 				sui_network: "testnet",
-				nbtc_pkg: "0xf30c0504bc24ffc4983669b931dbe869288da7a8afb88d8181cb307eda8b73cd",
-				nbtc_contract: "0x05661455295929e36d6419fba5b941f859a27e83eb4eebffa7a12e09022303b4",
+				nbtc_pkg: "0xddb6abfdb07259acac267d8e10db0bfa477f43c528356894c61ee0b8c0a77faf",
+				nbtc_contract: "0xed6ab4f97ddec4a6b8181292dea85319fd10250f5f113d04c2c404b254f47526",
 				lc_pkg: "0x808157392513cbc6034720c781b6d4360762a2a987ac4a4cc878c766272b1247",
 				lc_contract: "0xfe02d9ec80523746fbd07e79fc15085295d06cbc140983ba4af1a3b9e00cdd50",
 				sui_fallback_address:
 					"0x529216e27401313e3513102f5706bafa9c5b44831867352ab111456dc1c5c4a7",
-				btc_address: "bcrt1qtnz03pfehxlz3xma8xhumqjk3es8c4ld5v6ggs",
+				btc_address: "bcrt1qkhsjfvnp0pnglhz6qywh6n8eqvr5ludsday0x2",
 			},
 			// in order to add more addresses to the same package, add another entry with the same package config
 		],

2f4cb37b9f086be7ed337e829b9b9279ac14e5de
Robert Zaremba <robert@zaremba.ch>
2026-01-16 20:41:23 +0100

    fix: btc redeem tx tracking
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- packages/sui-indexer/src/rpc-interface.ts
+++ packages/sui-indexer/src/rpc-interface.ts
@@ -3,6 +3,6 @@ import type { RedeemRequestEventRaw } from "./models";
 export interface SuiIndexerRpc {
 	finalizeRedeem: () => Promise<void>;
 	putRedeemTx: (setupId: number, suiTxId: string, e: RedeemRequestEventRaw) => Promise<void>;
-	getBroadcastedRedeemTxIds: () => Promise<string[]>;
+	getBroadcastedRedeemTxIds: (setupId: number) => Promise<string[]>;
 	confirmRedeem: (txIds: string[], blockHeight: number, blockHash: string) => Promise<void>;
 }

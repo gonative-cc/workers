f9ece2eddff6c76a6d2ca815c8f0c5c0b8c0bff0
Robert Zaremba <robert@zaremba.ch>
2026-01-23 15:25:10 +0100

    update tests
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- packages/lib/src/test-helpers/init_db.test.ts
+++ packages/lib/src/test-helpers/init_db.test.ts
@@ -2,6 +2,7 @@ import { test, expect, afterAll } from "bun:test";
 import { Miniflare } from "miniflare";
 
 import { dropTables, initDb, tables } from "./init_db";
+import type { D1Database } from "@cloudflare/workers-types";
 
 const mf = new Miniflare({
 	script: "",
@@ -10,19 +11,26 @@ const mf = new Miniflare({
 	d1Persist: false,
 });
 
+async function selectTables(db: D1Database) {
+	const { results } = await db
+		.prepare("SELECT name FROM sqlite_schema WHERE type='table' AND name NOT LIKE 'sqlite_%'")
+		.all();
+	return results;
+}
+
 test("initDB", async () => {
 	const db = await mf.getD1Database("DB");
 	await initDb(db);
 
-	const { results } = await db
-		.prepare("SELECT name FROM sqlite_schema WHERE type='table' AND name NOT LIKE 'sqlite_%'")
-		.all();
-
-	const tableNames = results.map((row) => row.name);
+	const allTables = await selectTables(db);
+	const tableNames = allTables.map((row) => row.name);
 	const tableNamesStr = "created tables: " + JSON.stringify(tableNames);
 	expect(tableNames.length, tableNamesStr).toBeGreaterThanOrEqual(tables.length);
 	expect(tableNames).toContain("setups");
 	expect(tableNames).toContain("indexer_state");
+
+	await dropTables(db);
+	expect(await selectTables(db)).toBeEmpty();
 });
 
 afterAll(async () => {

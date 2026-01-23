a78cedbbb2b02e5c7fae2f0d46bb1f971bf8312a
Robert Zaremba <robert@zaremba.ch>
2026-01-23 13:21:49 +0100

    more tests
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- packages/lib/src/test-helpers/init_db.ts
+++ packages/lib/src/test-helpers/init_db.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import { readdir } from "fs/promises";
 import { D1Database } from "@cloudflare/workers-types";
+import assert from "assert";
 
 // Loads all migration files into database
 export async function applyMigrations(db: D1Database, migrationsPath: string) {
@@ -11,19 +12,24 @@ export async function applyMigrations(db: D1Database, migrationsPath: string) {
 		if (filename.endsWith(".sql")) {
 			const file = Bun.file(path.join(migrationsPath, filename));
 			const migration = await file.text();
+			// we need to put each statement in a single line
 			const cleanedMigration = migration
 				.replace(/--.*/g, "")
 				.replace(/\n/g, " ")
 				.replace(/\s{2,}/g, " ")
+				// separate each statement in a new line to have a correct result.count
+				.replace(/;/g, "\n")
 				.trim();
-			if (cleanedMigration.length > 0) {
-				await db.exec(cleanedMigration);
-			}
+			assert(cleanedMigration.length > 0, "empty migration: " + filename);
+			const result = await db.exec(cleanedMigration);
+			const numCreate = cleanedMigration.match(/\bcreate\b/gi)?.length || 0;
+			assert(result.count > 0, "migrations execution failed");
+			assert(result.count === numCreate, "migrations execution failed");
 		}
 	}
 }
 
-const MIGRATIONS_PATH = path.resolve(__dirname, "../../btcindexer/db/migrations");
+const MIGRATIONS_PATH = path.resolve(__dirname, "../../../btcindexer/db/migrations");
 // NOTE: Drop tables in correct order: child tables first to avoid foreign key constraints
 const tables = [
 	"nbtc_redeem_solutions",

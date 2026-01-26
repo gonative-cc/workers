28c2e08619e32a216dfe0549bb00eb4533dcdc78
Robert Zaremba <robert@zaremba.ch>
2026-01-26 18:29:11 +0100

    move validateSetups
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- packages/lib/src/setups.test.ts
+++ packages/lib/src/setups.test.ts
@@ -1,5 +1,32 @@
 import { test, expect } from "bun:test";
-import { setupEnvs, validateAllSetups } from "./setups";
+import { isValidSuiAddress, isValidSuiObjectId } from "@mysten/sui/utils";
+
+import { type Setup, setupEnvs } from "./setups";
+
+// checks:
+// - every id is unique
+// - all contracts and pkgs are valid sui addresses
+function validateAllSetups(setupMaps: Record<string, Setup[]>): Error | null {
+	const ids = new Set();
+	const errs: string[] = [];
+	for (const [_envName, setups] of Object.entries(setupMaps)) {
+		for (const s of setups) {
+			if (ids.has(s.id)) errs.push(`Setup id=${s.id} is not unique`);
+			else ids.add(s.id);
+
+			if (!isValidSuiObjectId(s.nbtc_contract))
+				errs.push(`Setup=${s.id} has invalid nbtc_contract`);
+			if (!isValidSuiObjectId(s.lc_contract))
+				errs.push(`Setup=${s.id} has invalid lc_contract`);
+			if (!isValidSuiAddress(s.nbtc_pkg)) errs.push(`Setup=${s.id} has invalid nbtc_pkg`);
+			if (!isValidSuiAddress(s.lc_pkg)) errs.push(`Setup=${s.id} has invalid lc_pkg`);
+		}
+	}
+	if (errs.length > 0) {
+		return new Error(JSON.stringify(errs));
+	}
+	return null;
+}
 
 test("orgSetups", () => {
 	expect(validateAllSetups(setupEnvs)).toBeNull();
--- packages/lib/src/setups.ts
+++ packages/lib/src/setups.ts
@@ -1,4 +1,3 @@
-import { isValidSuiAddress, isValidSuiObjectId } from "@mysten/sui/utils";
 import { BtcNet } from "./nbtc";
 import type { SuiNet } from "./nsui";
 
@@ -70,29 +69,4 @@ export function getActiveSetups(envName: string): Setup[] {
 
 export function getSetup(id: number): Setup | undefined {
 	return setupsById[id];
-}
-
-// checks:
-// - every id is unique
-// - all contracts and pkgs are valid sui addresses
-export function validateAllSetups(setupMaps: Record<string, Setup[]>): Error | null {
-	const ids = new Set();
-	const errs: string[] = [];
-	for (const [_envName, setups] of Object.entries(setupMaps)) {
-		for (const s of setups) {
-			if (ids.has(s.id)) errs.push(`Setup id=${s.id} is not unique`);
-			else ids.add(s.id);
-
-			if (!isValidSuiObjectId(s.nbtc_contract))
-				errs.push(`Setup=${s.id} has invalid nbtc_contract`);
-			if (!isValidSuiObjectId(s.lc_contract))
-				errs.push(`Setup=${s.id} has invalid lc_contract`);
-			if (!isValidSuiAddress(s.nbtc_pkg)) errs.push(`Setup=${s.id} has invalid nbtc_pkg`);
-			if (!isValidSuiAddress(s.lc_pkg)) errs.push(`Setup=${s.id} has invalid lc_pkg`);
-		}
-	}
-	if (errs.length > 0) {
-		return new Error(JSON.stringify(errs));
-	}
-	return null;
 }
